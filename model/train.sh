57b109ce1ec0b776c833e293fabc45c6329345d1
Cameron Sun <cameron.csun@gmail.com>
2017-08-29 19:44:03 +0000

    Final commit

--- model/train.sh
+++ model/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-	/SegNet/caffe-segnet-cudnn5/build/tools/caffe train -gpu 0 -solver /SegNet/models/nyu_segnet_solver.prototxt
+	/SegNet/caffe-segnet-cudnn5/build/tools/caffe train -gpu 0 -solver /SegNet/models/combined_solver.prototxt
 else
-	/SegNet/caffe-segnet-cudnn5/build/tools/caffe train -gpu 0 -solver /SegNet/models/nyu_segnet_solver.prototxt -snapshot $1
+	/SegNet/caffe-segnet-cudnn5/build/tools/caffe train -gpu 0 -solver /SegNet/models/combined_solver.prototxt -snapshot $1
 fi
